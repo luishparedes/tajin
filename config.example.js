6305b03608c859c13d1859e697ae85890ca201d3
luishparedes <luishparedes94@gmail.com>
2025-11-02 11:17:00 -0400

    Delete config.example.js

--- config.example.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAKR4EVDjVV944_jZg_8vGu9Ge7mIxMK4g",
-    authDomain: "magicaservidor.firebaseapp.com",
-    projectId: "magicaservidor",
-    storageBucket: "magicaservidor.firebasestorage.app",
-    messagingSenderId: "842627626709",
-    appId: "1:842627626709:web:794726b1d35317af81145c",
-    databaseURL: "https://magicaservidor-default-rtdb.firebaseio.com/"
-};
